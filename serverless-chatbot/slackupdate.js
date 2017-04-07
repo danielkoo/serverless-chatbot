bbdc6944321ee32e8ffaee93480442ab13a1ec28
Daniel Koo <koo.daniel@gmail.com>
2017-04-07 15:47:43 +1000

    it's now able to return a shortened URL which points to the converted image

--- serverless-chatbot/slackupdate.js
+++ serverless-chatbot/slackupdate.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const request = require('request');
+
 const aws = require('aws-sdk');
 const https = require('https');
 const qs = require('querystring');
@@ -15,6 +17,27 @@ const getSignedUrl = function(bucket, key) {
     };
     var url = s3.getSignedUrl('getObject', params);
     resolve(url);
+  });
+};
+
+const getShortUrl = function(url) {
+  return new Promise((resolve, reject) => {
+    var req = {
+      uri: process.env.SHORTENER_API_URL + qs.stringify({key: process.env.SHORTENER_API_KEY}),
+      method: 'POST',
+      json: true,
+      body: {
+        longUrl: url
+      }
+    };
+
+    request(req, (err, res, body) => {
+      if (err && res.statusCode !== 200) {
+        reject(err);
+      } else {
+        resolve(body.id);
+      }
+    });
   });
 };
 
@@ -41,6 +64,7 @@ module.exports.execute = (event, context, callback) => {
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
 
   getSignedUrl(bucket, key)
+    .then((url) => getShortUrl(url))
     .then((url) => writeToSlack(url))
     .then(() => callback(null))
     .catch((err) => callback(err));
